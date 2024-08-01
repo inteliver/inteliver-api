53ca2ab724a34dabb7a33ef738d062250449b08b
Amir Lavasani <amirm.lavasani@gmail.com>
2024-08-01 12:26:30 +0330

    fix: add storage to routers

--- src/app/routers/__init__.py
+++ src/app/routers/__init__.py
@@ -8,6 +8,7 @@ from fastapi import FastAPI
 
 from app.auth.router import router as auth_router
 from app.config import settings
+from app.storage.router import router as storage_router
 from app.users.router import router as users_router
 from app.utils.i18n import _
 
@@ -15,6 +16,8 @@ from .version_router import router as version_router
 
 
 def register_routers(app: FastAPI):
+    app.include_router(storage_router, prefix=f"{settings.api_prefix}/storage")
+
     app.include_router(auth_router, prefix=f"{settings.api_prefix}/auth")
 
     app.include_router(users_router, prefix=f"{settings.api_prefix}/users")
