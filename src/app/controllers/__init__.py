63e54011e9baa12797beee43aab78bcd8da6481a
Amir Lavasani <amirm.lavasani@gmail.com>
2024-08-01 12:25:58 +0330

    refactor: remove unused folders

--- src/app/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-# This section is the controller section of the service.
-# All logics will be implemented in this section.
-# Routers will route the request to their corosponding controller and return the results.
--- src/app/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# This section is for any AI models that this service uses
-# AI Model definition or any other utils related to the AI service will be here.
